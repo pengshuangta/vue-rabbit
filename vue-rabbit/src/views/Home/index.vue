414c80244102bf27c3a815cf52c7b7a5307705b5
pst <1120160658@qq.com>
2024-10-17 16:09:50 +0800

    面板组件封装

--- vue-rabbit/src/views/Home/index.vue
+++ vue-rabbit/src/views/Home/index.vue
@@ -5,6 +5,7 @@ import HomeBanner from "./components/HomeBanner.vue"
 import HomeNew from "./components/HomeNew.vue"
 import HomeHot from "./components/HomeHot.vue"
 import HomeProduct from "./components/HomeProduct.vue"
+import HomePanel from "./components/HomePanel.vue"
 
 </script>
 
@@ -16,5 +17,19 @@ import HomeProduct from "./components/HomeProduct.vue"
     <HomeNew />
     <HomeHot />
     <HomeProduct />
+
+    <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+        <div>
+            这是新鲜好物的插槽
+        </div>
+    </HomePanel>
+    
+
+    <HomePanel title="人气推荐" sub-title="热门推荐 好多商品">
+       <div>
+            这是新鲜好物的插槽
+        </div>
+    </HomePanel>
+     
     
 </template>
