8909646e47a8f88abc843b34d90b027098902e91
pst <1120160658@qq.com>
2024-10-18 14:45:22 +0800

    懒加载指令优化

--- vue-rabbit/src/main.js
+++ vue-rabbit/src/main.js
@@ -7,8 +7,8 @@ import router from './router'
 
 // 引入初始化样式文件
 import '@/styles/common.scss'
+import { lazyPlugin } from './directives'
 
-import { useIntersectionObserver } from '@vueuse/core'
 
 // 测试接口函数
 // import {getCategory} from '@/apis/testAPI'
@@ -17,34 +17,11 @@ import { useIntersectionObserver } from '@vueuse/core'
 // })
 
 const app = createApp(App)
-
+app.use(lazyPlugin)
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
 
-// 定义全局指令
-app.directive('img-lazy',{
-    mounted(el,binding){
-        // el:当前的img元素
-        // binging数据值,即src
-        console.log(el,binding)
-        // useIntersectionObserver(el,([{isIntersecting}]) => {
-        //     console.log(isIntersecting)
-        // })
-
-        const { stop } = useIntersectionObserver(
-            el,
-            ([{ isIntersecting }]) => {
-              console.log(isIntersecting)
-              if (isIntersecting) {
-                // 进入视口区域
-                el.src = binding.value
-                stop()
-              }
-            },
-        )
-    }
-})
 
 
