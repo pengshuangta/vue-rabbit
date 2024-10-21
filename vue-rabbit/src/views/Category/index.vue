830928e726286e8899d6155ecd2524a1db660e7f
pst <1120160658@qq.com>
2024-10-21 11:12:52 +0800

    激活状态显示和分类渲染

--- vue-rabbit/src/views/Category/index.vue
+++ vue-rabbit/src/views/Category/index.vue
@@ -3,6 +3,7 @@ import { getCategoryAPI } from "@/apis/category";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import { getBannerAPI } from "@/apis/home";
+import GoodsItem from "@/views/Home/components/GoodsItem.vue"
 
 // 获取分类
 const route = useRoute();
@@ -47,6 +48,30 @@ onMounted(() => {
             <img :src="item.imgUrl" alt="" />
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <!-- 分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div
+        class="ref-goods"
+        v-for="item in categoryData.children"
+        :key="item.id"
+      >
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
@@ -130,15 +155,15 @@ onMounted(() => {
     padding: 25px 0;
   }
   .home-banner {
-  width: 1240px;
-  height: 500px;
-  margin: 0 auto;
-  z-index: 98;
-
-  img {
-    width: 100%;
+    width: 1240px;
     height: 500px;
+    margin: 0 auto;
+    z-index: 98;
+
+    img {
+      width: 100%;
+      height: 500px;
+    }
   }
-}
 }
 </style>
