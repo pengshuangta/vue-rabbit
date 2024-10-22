437b37a547f393c29026968f1df192cdffb33338
pst <1120160658@qq.com>
2024-10-22 10:01:44 +0800

    详情页- 基础数据渲染

--- vue-rabbit/src/views/Detail/index.vue
+++ vue-rabbit/src/views/Detail/index.vue
@@ -1,17 +1,35 @@
 <script setup>
 
+import {getDetailAPI} from '@/apis/detail'
+import { onMounted, ref } from 'vue'
+import {useRoute} from 'vue-router'
+
+// 获取详情接口数据
+const route = useRoute()
+const detailData = ref({})
+const getDetail = async() =>{
+    const res = await getDetailAPI(route.params.id)
+    console.log(res)
+    detailData.value = res.result
+}
+onMounted(() => getDetail())
 
 </script>
 
 <template>
   <div class="xtx-goods-page">
-    <div class="container">
+    <div class="container" v-if="detailData.details">
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">母婴
+          <!-- 
+            错误原因：goods一开始{}  {}.categories -> undefined  -> undefined[1]
+            1. 可选链的语法?. 
+            2. v-if手动控制渲染时机 保证只有数据存在才渲染
+           -->
+          <el-breadcrumb-item :to="{ path: `/category/${detailData.categories[1].id}` }">{{detailData.categories[1].name}}
           </el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">跑步鞋
+          <el-breadcrumb-item :to="{ path: `/category/${detailData.categories[0].id}` }">{{detailData.categories[0].name}}
           </el-breadcrumb-item>
           <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
         </el-breadcrumb>
@@ -27,33 +45,33 @@
               <ul class="goods-sales">
                 <li>
                   <p>销量人气</p>
-                  <p> 100+ </p>
+                  <p> {{detailData.salesCount}}+ </p>
                   <p><i class="iconfont icon-task-filling"></i>销量人气</p>
                 </li>
                 <li>
                   <p>商品评价</p>
-                  <p>200+</p>
+                  <p>{{detailData.commentCount}}+</p>
                   <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
                 </li>
                 <li>
                   <p>收藏人气</p>
-                  <p>300+</p>
+                  <p>{{detailData.collectCount}}+</p>
                   <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <p>400+</p>
+                  <p>{{detailData.brand.name}}</p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
             </div>
             <div class="spec">
               <!-- 商品信息区 -->
-              <p class="g-name"> 抓绒保暖，毛毛虫儿童鞋 </p>
-              <p class="g-desc">好穿 </p>
+              <p class="g-name"> {{detailData.name}} </p>
+              <p class="g-desc">{{detailData.desc}} </p>
               <p class="g-price">
-                <span>200</span>
-                <span> 100</span>
+                <span>{{detailData.oldPrice}}</span>
+                <span> {{detailData.price}}</span>
               </p>
               <div class="g-service">
                 <dl>
@@ -93,13 +111,13 @@
                 <div class="goods-detail">
                   <!-- 属性 -->
                   <ul class="attrs">
-                    <li v-for="item in 3" :key="item.value">
-                      <span class="dt">白色</span>
-                      <span class="dd">纯棉</span>
+                    <li v-for="item in detailData.details.properties" :key="item.value">
+                      <span class="dt">{{item.name}}</span>
+                      <span class="dd">{{item.value}}</span>
                     </li>
                   </ul>
                   <!-- 图片 -->
-
+                  <img v-for="img in detailData.details.pictures" :src="img" :key="img" alt="">
                 </div>
               </div>
             </div>
